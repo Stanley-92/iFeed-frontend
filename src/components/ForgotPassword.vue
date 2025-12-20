dfa0194c813e69a88e9359819d881985dce0d263
Stanley-92 <leystan405@gmail.com>
2025-12-20 18:14:39 +0700

    Update

---
+++ src/components/ForgotPassword.vue
@@ -0,0 +1,29 @@
+<script>
+import { sendPasswordResetEmail } from 'firebase/auth';
+import { auth } from '../firebase';
+
+export default {
+  data() {
+    return {
+      email: ''
+    };
+  },
+  methods: {
+    async resetPassword() {
+      if (!this.email) {
+        alert('Please enter your email address.');
+        return;
+      }
+
+      try {
+        await sendPasswordResetEmail(auth, this.email);
+        alert('Password reset email sent! Please check your inbox.');
+        this.$router.push('/');
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+  }
+};
+</script>
+
