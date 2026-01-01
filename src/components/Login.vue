87528c349e00273220c1d1bc290503e6f48aede1
Stanley-92 <leystan405@gmail.com>
2026-01-01 21:20:58 +0700

    Updated

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,10 +3,12 @@
     <div class="flex w-full max-w-6xl items-center justify-between px-8 gap-10">
 
       <!-- LEFT TEXT -->
-<div class="flex-1 min-h-[100dvh] mt-8 bg-blue-100 ">
+
+
+<div class="flex min-h-[100dvh] mt-8  ">
 <aside >
 <div class=" w-65 hidden md:block max-w-full">
-<button class="flex-1 flex-col items-center text-sm item-center mt-4  inset-y-0 left-0 flex items-center pl-3 cursor-pointer ">
+<button class="flex-1 items-center pl-3 cursor-pointer ">
 <Icon icon="tdesign:chat-bubble" 
 class="w-14 h-14 text-white transition-colors bg-green-500 border-4 border-green-500 rounded-xl duration-200 hover:text-gray-600 mb-10" 
  @click="goToMainfeed"/>
@@ -20,37 +22,26 @@ class="w-14 h-14 text-white transition-colors bg-green-500 border-4 border-green
         </p>
 </div>
 
-
-      <!-- ACTION + STORY STACK -->
-      <div class="relative items-center mt-10 ">
-
-        <!-- ACTION MENU -->
+   <!-- ACTION MENU -->
         <div class="flex flex-col gap-6 text-black">
           <div class="flex items-center gap-3">
-            <Icon icon="" class="w-7 h-7" />
-            <span class="text-xl font-semibold">Chat</span>
           </div>
-
+      
           <div class="flex items-center gap-3">
-            <Icon icon="" class="w-7 h-7" />
-            <span class="text-xl font-semibold">Like</span>
-          </div>
-
-          <div class="flex items-center gap-3">
-            <Icon icon="" class="w-7 h-7" />
-            <span class="text-xl font-semibold">Comment</span>
-          </div>
-
-          <div class="flex items-center gap-3">
-        <icon icon="uil:comment" class="w-10 h-10"/>
+           <Icon icon="uil:comment" class="w-10 h-10"/>
             <span class="text-xl font-semibold">Share</span>
           </div>
         </div>
 
       </div>
 
+      <!-- ACTION + STORY STACK -->
+      <div class="relative items-center mt-10 p-2  ">
+
+     
+
     <!-- CENTER STORY STACK -->
-      <div class="relative   items-center w-[360px] h-[320px] mt-50  p-2 ">
+      <div class="relative   items-center w-[360px] h-[320px] mt-50 p-2 ">
         <img
           v-for="(story, i) in stories"
           :key="i"
@@ -77,7 +68,7 @@ class="w-14 h-14 text-white transition-colors bg-green-500 border-4 border-green
 
 
       <!-- LOGIN FORM -->
-      <div class="bg-gray p-8 rounded-lg shadow-md w-full max-w-sm border mb-50">
+      <div class="bg-gray p-8 rounded-xl shadow-md w-full max-w-sm border mb-50">
         <input
           type="text"
           placeholder="Email or Phone number"
@@ -98,8 +89,7 @@ class="w-14 h-14 text-white transition-colors bg-green-500 border-4 border-green
 
         <router-link
           to="/forgot-password"
-          class="text-sm text-center text-gray-500 mb-4 block"
-        >
+          class="text-sm text-center text-gray-500 mb-4 block">
           Forgot your password?
         </router-link>
 
@@ -107,8 +97,7 @@ class="w-14 h-14 text-white transition-colors bg-green-500 border-4 border-green
 
         <router-link to="/create" class="block">
           <button
-            class="bg-green-600 text-white w-full py-2 rounded text-sm font-semibold hover:bg-green-700"
-          >
+            class="bg-green-600 text-white w-full py-2 rounded text-sm font-semibold hover:bg-green-700">
             Create your account
           </button>
         </router-link>
@@ -146,9 +135,10 @@ export default {
       password: '',
       loading: false,
       stories: [
+          { img: story2, rotate: '-14deg', right: '70px', z: 2 },
         { img: story1, rotate: '12deg', left: '300px', z: 1},
-        { img: story2, rotate: '-14deg', right: '70px', z: 2 },
-        { img: story3, rotate: '-5deg', left: '168px', z: 1 }
+      
+        { img: story3, rotate: '-2deg', left: '168px', z: 1 }
       ]
     }
   },
@@ -225,7 +215,7 @@ export default {
 }
 .story-card {
   @apply absolute top-0
-  w-[240px] h-[360px]
+  w-[148px] h-[240px]
   rounded-2xl
   object-cover
   shadow-[0_20px_40px_rgba(0,0,0,0.78)]
