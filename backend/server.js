a370a32dfb872d7a7672dde134a155236c1b7432
Stanley-92 <leystan405@gmail.com>
2026-01-07 13:24:39 +0700

    iFeed-frontend

--- backend/server.js
+++ backend/server.js
@@ -89,8 +89,8 @@ app.post('/check-verified', async (req, res) => {
 })
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
